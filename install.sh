bd51c2b7a1bc8cbc69a1cb3aecb21b2ea656d239
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 19:37:43 -0300

    Refactor install.sh for improved messaging and structure

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 echo [MakT Created By Blueskyteam] [Repo in Abril 2026]
 sleep 2
-echo [*] ALERT: Use Termux because otherwise the commands will be incompatible!
+echo [] ALERT: Use Termux because otherwise the commands will be incompatible!
 sleep 2
-echo [*] Creating Module SH...
+echo [] Creating Module SH...
 sleep 1
 echo [*] in case exists file, delete file
 sleep 2
 rm -rf $PREFIX/bin/MakT 2>/dev/null
 sleep 1
-cat <<EOF>> $PREFIX/bin/MakT
-echo [*] Personalizing terminal...
-read -p "What You Name?: " name
-cat <<EOF>> ~/.bashrc
-clear
-echo [Blueskyteam Plugin | 
-export PS1="MakT@$NAME "
-EOF
-
-EOF
+cat <<'OUTER' > $PREFIX/bin/MakT
+echo [] Personalizing terminal...
+read -p "What Your Name?: " name
+echo "clear" >> ~/.bashrc
+echo "echo [Blueskyteam Plugin]" >> ~/.bashrc
+echo "export PS1="MakT@$name $ "" >> ~/.bashrc
+echo "[] Prompt configurado! Reinicie o Termux."
+OUTER
 sleep 5
-echo [*] Adding execute permission...
+echo [] Adding execute permission...
 chmod +x $PREFIX/bin/MakT
 sleep 1
-echo [*] Installed!
+echo [] Installed!
 sleep 2
 echo [Note] Type MakT to run.
