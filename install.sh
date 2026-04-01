3aa41e55ce16a91fd30d3e206ac32e3cdea548d6
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 19:52:28 -0300

    Add color personalization to terminal setup script

--- install.sh
+++ install.sh
@@ -11,12 +11,34 @@ cat << 'EOF' > $PREFIX/bin/MakT
 #!/data/data/com.termux/files/usr/bin/sh
 echo "[*] Personalizing terminal..."
 read -p "What Your Name?: " name
+read -p "What's your favorite color?: " color
+
+case $color in
+    gray) col="\[\e[1;30m\]"; bg_col="\[\e[0;30m\]" ;;
+    "light blue") col="\[\e[1;36m\]"; bg_col="\[\e[0;36m\]" ;;
+    "dark blue") col="\[\e[1;34m\]"; bg_col="\[\e[0;34m\]" ;;
+    white) col="\[\e[1;37m\]"; bg_col="\[\e[0;37m\]" ;;
+    purple) col="\[\e[1;35m\]"; bg_col="\[\e[0;35m\]" ;;
+    pink) col="\[\e[1;35m\]"; bg_col="\[\e[0;35m\]" ;;
+    violet) col="\[\e[1;35m\]"; bg_col="\[\e[0;35m\]" ;;
+    red) col="\[\e[1;31m\]"; bg_col="\[\e[0;31m\]" ;;
+    yellow) col="\[\e[1;33m\]"; bg_col="\[\e[0;33m\]" ;;
+    orange) col="\[\e[1;33m\]"; bg_col="\[\e[0;33m\]" ;;
+    "calm blue") col="\[\e[1;36m\]"; bg_col="\[\e[0;36m\]" ;;
+    *) col="\[\e[1;37m\]"; bg_col="\[\e[0;37m\]" ;;
+esac
+
+reset="\[\e[0m\]"
+
 echo "clear" >> ~/.bashrc
 echo "echo [Blueskyteam Plugin]" >> ~/.bashrc
-echo "export PS1=\"\$name@MakT \$ \"" >> ~/.bashrc
+# O Nome fica com 'col' (vibrante) e o @MakT fica com 'bg_col' (versão mais escura/cinza)
+echo "export PS1=\"$col\$name$bg_col@MakT$reset \$ \"" >> ~/.bashrc
 exec bash
 EOF
 
 chmod +x $PREFIX/bin/MakT
+sleep 1
 echo [*] Installed!
+sleep 2
 echo [Note] Type MakT to run.
