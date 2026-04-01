f7f77779d816f4a35e8a9faf3e275d41f9ec1a00
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 19:07:08 -0300

    Modify install.sh for Termux compatibility
    Updated directory names and added alert for Termux usage.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 echo [Thl Created By Blueskyteam] [Repo in Abril 2026]
 sleep 2
+echo [*] ALERT: Use Termux because otherwise the commands will be incompatible!
+sleep 2
 echo [*] Creating Module SH...
 sleep 1
 echo [*] in case exists file, delete file
 sleep 2
-rm -rf $PREFIX/bin/Thl2>/dev/null
+rm -rf $PREFIX/bin/Thl 2>/dev/null
 sleep 1
 cat <<EOF>> $PREFIX/bin/Thl
 echo [*] Allow him to do what he commands.
@@ -12,8 +14,8 @@ sleep 3
 termux-setup-storage
 sleep 2
 mkdir /sdcard/Thl2>/dev/null
-mkdir /sdcard/Thl/cache2>/dev/null
-mkdir /sdcard/Thl/files2>/dev/null
+mkdir /sdcard/Thl/cache 2>/dev/null
+mkdir /sdcard/Thl/files 2>/dev/null
 EOF
 sleep 5
 echo [*] Adding execute permission...
