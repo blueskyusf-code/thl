eca9c0df579d183f7c289059f9a7f27e5ef72ec4
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 21:58:50 -0300

    Refactor install.sh for better readability and functionality

--- install.sh
+++ install.sh
@@ -1,46 +1,53 @@
-echo [MakT Created By Blueskyteam] [Repo in Abril 2026]
+#!/data/data/com.termux/files/usr/bin/bash
+echo "[MakT Created By Blueskyteam] [Repo in Abril 2026]"
 sleep 2
-echo [*] ALERT: Use Termux because otherwise the commands will be incompatible!
+echo "[*] ALERT: Use Termux because otherwise the commands will be incompatible!"
 sleep 2
-echo [*] Creating Module SH...
+echo "[*] Creating Module SH..."
 sleep 1
+
+# Remove versão antiga do MakT
 rm -rf $PREFIX/bin/MakT 2>/dev/null
 sleep 1
 
-cat << 'EOF' > $PREFIX/bin/MakT
-#!/data/data/com.termux/files/usr/bin/sh
+# Cria o novo script MakT
+cat << EOF > $PREFIX/bin/MakT
+#!/data/data/com.termux/files/usr/bin/bash
+
 echo "[*] Personalizing terminal..."
-read -p "What Your Name?: " name
+read -p "What is your name?: " name
 read -p "What's your favorite color?: " color
 
-case $color in
-    cinza) col="\[\033[1;30m\]" ;;
-    azul-claro) col="\[\033[1;36m\]" ;;
-    azul-escuro) col="\[\033[0;34m\]" ;;
-    branco) col="\[\033[1;37m\]" ;;
-    roxo) col="\[\033[0;35m\]" ;;
-    rosa) col="\[\033[1;35m\]" ;;
-    violeta) col="\[\033[1;35m\]" ;;
-    vermelho) col="\[\033[1;31m\]" ;;
-    amarelo) col="\[\033[1;33m\]" ;;
-    laranja) col="\[\033[0;33m\]" ;;
-    azul-calmo) col="\[\033[0;36m\]" ;;
-    *) col="\[\033[1;37m\]" ;;
+case \$color in
+    cinza) col="\\[\033[1;30m\\]" ;;
+    azul-claro) col="\\[\033[1;36m\\]" ;;
+    azul-escuro) col="\\[\033[0;34m\\]" ;;
+    branco) col="\\[\033[1;37m\\]" ;;
+    roxo) col="\\[\033[0;35m\\]" ;;
+    rosa) col="\\[\033[1;35m\\]" ;;
+    violeta) col="\\[\033[1;35m\\]" ;;
+    vermelho) col="\\[\033[1;31m\\]" ;;
+    amarelo) col="\\[\033[1;33m\\]" ;;
+    laranja) col="\\[\033[0;33m\\]" ;;
+    azul-calmo) col="\\[\033[0;36m\\]" ;;
+    *) col="\\[\033[1;37m\\]" ;;
 esac
 
-reset="\[\033[0m\]"
+reset="\\[\033[0m\\]"
 
 # Configura o prompt permanentemente
 echo "clear" >> ~/.bashrc
 echo "echo [Blueskyteam Plugin]" >> ~/.bashrc
-echo "export PS1=\"$col\$name$reset@MakT \$ \"" >> ~/.bashrc
+echo "export PS1=\"\$col\$name\$reset@MakT \$ \"" >> ~/.bashrc
 
 # Aplica a mudança na sessão atual
 exec bash
 EOF
 
+# Torna o script executável
 chmod +x $PREFIX/bin/MakT
 sleep 1
-echo [*] Installed!
+
+echo "[*] Installed!"
 sleep 2
-echo [Note] Type MakT to run.
+echo "[Note] Type MakT to run."
